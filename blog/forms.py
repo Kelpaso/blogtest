b06ce189a6e4605346b5758946915dc0520f8bd4
heldstory <heldstory@gmail.com>
2024-01-19 21:59:47 +0300

    comments on post page first

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Post
+from .models import Post, Comment
 
 class PostForm(forms.ModelForm):
 
@@ -10,3 +10,9 @@ class PostForm(forms.ModelForm):
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=65)
     password = forms.CharField(max_length=65, widget=forms.PasswordInput)
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ('author', 'text')
